0f9e1aad5077a5b066000abbae50097daace7918
imayworm <51038779+imayworm@users.noreply.github.com>
2022-06-01 21:22:40 -0300

    iframe

---
+++ dark-mode.js
@@ -0,0 +1,47 @@
+function darkMode() {
+    document.getElementsByClassName(".menu").
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function setColorScheme(scheme) {
+    switch(scheme){
+        case 'dark':
+            // Dark
+            break;
+        case 'light':
+            // Light
+            break;
+        default:
+            // Default
+            break;
+    }
+}
+
+function getPreferredColorScheme() {
+    if (window.matchMedia) {
+        if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+            return 'dark';
+        } else {
+            return 'light';
+        }
+    }
+    return 'light';
+}
+
+if(window.matchMedia){
+    var colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    colorSchemeQuery.addEventListener('change', setColorScheme(getPreferredColorScheme()));
+}*/
